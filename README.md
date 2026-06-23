bd959f322f964ae1470ff905ed90f2a46a521bb9
Tyler Nijmeh <tylernij@gmail.com>
2026-06-23 17:09:49 -0500

    Working files
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # fffrag
+
 FAT folder-first defragmentation to position directories at low cluster positions.
+
+# Why?
+
+Some Spreadstrum SoCs in feature phones have a firmware bug when reading FAT filesystems which prevents them from reading a directory if it appears at too large of a sector number. The device may show "Empty folder", even when the directory is populated. Duplicating a working directory will still result in this error, because the sector number is too large. Files, however, are unaffected. Therefore, we must allocate the directory structures first, and then copy the files after. This results in small sector numbers for the directories.
+
+# Usage
+
+## fffrag
+
+Defragment and reorganize the FAT structure to position directories at the beginning of the FAT table.
+
+Usage: `fffrag /dev/sdX1`
+
+### fffcopy
+
+Recursively copy a directory from the host system to the FAT device, directories first.
+
+Usage: `fffcopy /dev/sdX1 /path/to/source/dir`
+
+### fffvalidate
+
+Print the FAT cluster and sector numbers for the final filesystem.
+
+Usage: `fffvalidate /dev/sdX1`
---
+++ fffrag.sh
@@ -0,0 +1,131 @@
+#!/usr/bin/env bash
+
+function fffvalidate() {
+    local disk="$1"
+
+    local mountpoint
+    mountpoint="$(lsblk -no MOUNTPOINT "$disk")"
+
+    while read -r dir; do
+        relpath="${dir#"$mountpoint"}"
+        relpath="${relpath:-/}"
+        echo "$relpath"
+        sudo fatcat "$disk" -l "$relpath" | grep "c="
+    done < <(find "$mountpoint" -type d)
+}
+
+function fffcopy() {
+    local disk="$1"
+    local source="$2"
+
+    local mountpoint
+    mountpoint="$(lsblk -no MOUNTPOINT "$disk")"
+
+    # Recreate FAT filesystem.
+    sudo umount "$disk"
+    sudo mkfs.fat -F 32 "$disk"
+
+    # Reestablish mountpoint.
+    sudo mkdir -p "$mountpoint"
+    sudo mount "$disk" "$mountpoint"
+
+    # Copy over directories.
+    local reldir
+    local destdir
+    while read -r dir; do
+        reldir="${dir#"$source"}"
+        destdir="$mountpoint$reldir"
+
+        # Create low-sector positioned directory.
+        sudo mkdir -p "$destdir"
+    done < <(find "$source" -mindepth 1 -type d | sort)
+
+    # Copy the files from source.
+    sudo rsync -rlhv "$source/" "$mountpoint/"
+
+    # Validate.
+    fffvalidate "$disk"
+}
+
+function fffrag() {
+    local disk="$1"
+    local tmpdir
+    tmpdir="$(mktemp -d tmp.XXXXXX)"
+
+    local mountpoint
+    mountpoint="$(lsblk -no MOUNTPOINT "$disk")"
+
+    # Move to local temp folder.
+    rsync -avh --remove-source-files "$mountpoint/" "$tmpdir/"
+
+    # Recreate FAT filesystem.
+    sudo umount "$disk"
+    sudo mkfs.fat -F 32 "$disk"
+
+    # Reestablish mountpoint.
+    sudo mkdir -p "$mountpoint"
+    sudo mount "$disk" "$mountpoint"
+
+    # Copy over directories.
+    local reldir
+    local destdir
+    while read -r dir; do
+        reldir="${dir#"$tmpdir"}"
+        destdir="$mountpoint$reldir"
+
+        # Create low-sector positioned directory.
+        sudo mkdir -p "$destdir"
+    done < <(find "$tmpdir" -mindepth 1 -type d | sort)
+
+    # Copy the files back.
+    sudo rsync -rlhv "$tmpdir/" "$mountpoint/"
+
+    # Clean up temp folder.
+    rm -rf "$tmpdir"
+
+    # Validate.
+    fffvalidate "$disk"
+}
+
+function main() {
+    local disk="$1"
+    
+    if [[ -z "$disk" ]]; then
+        echo "Incorrect usage."
+        exit 1
+    fi
+
+    if [[ ! -b "$disk" ]]; then
+        echo "$disk is not a valid disk."
+        exit 1
+    fi
+
+    local mountpoint
+    mountpoint="$(lsblk -no MOUNTPOINT "$disk")"
+    if [[ -z "$mountpoint" ]]; then
+        echo "$disk has no valid mountpoint."
+        exit 1
+    fi
+
+    echo "DISK:          $disk"
+    echo "MOUNTPOINT:    $mountpoint"
+    echo
+
+    local ok
+    echo "WARNING: $disk will be COMPLETELY erased!"
+    read -rn 1 -p "Proceed [yn]:" ok
+    echo
+
+    if [[ "$ok" != "y" ]]; then
+        echo "Exiting."
+        exit 1
+    fi
+
+    # Execute.
+    fffrag "$disk"
+}
+
+if [[ "${BASH_SOURCE[0]}" == "$0" ]]; then
+    [[ $EUID -ne 0 ]] && exec sudo "$0" "$@"
+    main "$@"
+fi
